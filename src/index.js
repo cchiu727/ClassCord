8f843529af86a7911b02b190594e512e87777d70
Collin Chiu <collinchiu23@gmail.com>
2023-04-08 17:57:44 -0700

    Modify index.js bot run

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-const { Client, IntentsBitField } = require('discord.js');
+// Discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+// get token from config.js
+const { token } = require('../config.json');
 
-const client = new Client(
-    {
-        intents: [
-            IntentsBitField.Flags.Guilds,
-            IntentsBitField.Flags.GuildMembers,
-            IntentsBitField.Flags.GuildMessages,
-            IntentsBitField.Flags.MessageContent,
-        ],
-    }
-);
+// create new client instance
+const client = new Client({ intents: [GatweayIntentBits.Guilds] });
+
+// log when client is ready
+client.once(Events.ClientReady, c => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with client token
+client.login(token);
